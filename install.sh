c466b7fdb23957f823e0081b704d74539bae2c3e
Joseph Le <joseph.le@apactech.io>
2023-09-25 20:11:50 +1000

    giteaHook service

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+if [ -d "/etc/systemd/system" ]; then
+  echo "|-------------------|"
+  echo "|***** INSTALL *****|"
+  echo "|-------------------|"
+
+  systemctl stop giteaHook.service
+  cp ./service/giteaHook.service /etc/systemd/system/giteaHook.service &&
+  systemctl daemon-reload &&
+  systemctl enable giteaHook &&
+  systemctl start giteaHook &&
+
+  echo "|------------|"
+  echo "|*** DONE ***|"
+  echo "|------------|"
+
+else
+  echo "/etc/systemd/system directory does not exist. Please check!"
+fi
