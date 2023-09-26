275ebc7ae65a73a654d87dbdb73c0c2faee7eedc
Joseph Le <joseph.le@apactech.io>
2023-09-26 07:21:02 +0000

    update environment auto

--- install.sh
+++ install.sh
@@ -5,20 +5,61 @@ if [ -d "/etc/systemd/system" ]; then
   echo "|-------------------|"
   echo "|***** INSTALL *****|"
   echo "|-------------------|"
+  
+  pwd_install=$(pwd) &&
+  escaped_pwd_install=$(printf '%s\n' "$pwd_install" | sed -e 's/[\/&]/\\&/g') &&
+ 
+  echo "|------------------------------|"
+  echo "|***** UPDATE ENVIRONMENT *****|"
+  echo "|------------------------------|"
+  
+  
+  sudo sed -i "s#ExecStart=.*#ExecStart=$escaped_pwd_install/service/giteaService.sh#" $pwd_install/service/giteaHook.service &&
+  echo "* ExecStart *" &&
+  sleep 3 && 
 
-  systemctl stop giteaHook.service
-  cp ./service/giteaHook.service /etc/systemd/system/giteaHook.service &&
+  sudo sed -i "s#WorkingDirectory=.*#WorkingDirectory=$escaped_pwd_install/service#" $pwd_install/service/giteaHook.service &&
+  echo "* WorkingDirectory *" &&
+  sleep 3 &&
+
+  sudo sed -i "s#EnvironmentFile=.*#EnvironmentFile=$escaped_pwd_install/service/giteaService#" $pwd_install/service/giteaHook.service &&
+  echo "* EnvironmentFile *" &&
+  sleep 3 &&
+
+  sudo sed -i "s#INDEXJS_HOOK_PATH=.*#INDEXJS_HOOK_PATH=$escaped_pwd_install#" $pwd_install/service/giteaService &&
+  echo "* INDEXJS_HOOK_PATH *" &&
+  sleep 3 &&
+
+  sudo sed -i "s#HOOK_PATH=.*#HOOK_PATH=$escaped_pwd_install#" $pwd_install/service/giteaService &&
+  echo "* HOOK_PATH *" &&
+  sleep 3 &&
+
+  systemctl stop giteaHook.service 
+  
+  cp $pwd_install/service/giteaHook.service /etc/systemd/system/giteaHook.service &&
+  echo "* Copy service file *" &&
+  
   systemctl daemon-reload &&
+  
+  echo "|--------------|" &&
+  echo "|*** ENABLE ***|" &&
+  echo "|--------------|" &&
+  
   systemctl enable giteaHook &&
+  
+  echo "|-------------|" &&
+  echo "|*** START ***|" &&
+  echo "|-------------|" &&
+  
   systemctl start giteaHook &&
 
-  echo "|------------|"
-  echo "|*** DONE ***|"
-  echo "|------------|"
+  echo "|------------|" &&
+  echo "|*** DONE ***|" &&
+  echo "|------------|" &&
   
-  echo "|--------------|"
-  echo "|** GOODLUCK **|"
-  echo "|--------------|"
+  echo "|--------------|" &&
+  echo "|** GOODLUCK **|" &&
+  echo "|--------------|" &&
 
   journalctl -u giteaHook -f
 
--- service/giteaService.sh
+++ service/giteaService.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Đường dẫn tới tệp index.js
-indexjs_path=$INDEXJS_HOOK_PATH
+indexjs_path=$INDEXJS_HOOK_PATH/index.js
 project_path=$HOOK_PATH 
 
 # Kiểm tra xem Node.js đã cài đặt chưa
 if ! command -v node &> /dev/null; then
   echo "Node.js chưa được cài đặt. Đang cài đặt Node.js..."
-  curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-  sudo apt-get install -y nodejs
+  curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&
+  sudo apt-get install nodejs -y && 
+  node -v
 else
   echo "Nodejs da duoc cai dat!" &&
   node -v
@@ -17,7 +18,7 @@ fi
 # Kiểm tra xem npm đã cài đặt chưa
 if ! command -v npm &> /dev/null; then
   echo "npm chưa được cài đặt. Đang cài đặt npm..."
-  sudo apt-get install -y npm
+  sudo apt-get install npm -y
 else
   echo "npm da duoc cai dat!" &&
   npm -v
